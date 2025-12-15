e4bb216322bac67005647c9577bf7705a10221a6
screensaverstvbsk-commits <screensaverstvbsk@gmail.com>
2025-12-15 15:00:02 +0530

    refactor: Update brand logo URLs
    Removes unnecessary `thumb/` paths from brand logo URLs for cleaner and more direct image sourcing.

--- components/BrandSection.tsx
+++ components/BrandSection.tsx
@@ -4,27 +4,27 @@ export const BrandSection: React.FC = () => {
   const brands = [
     { 
       name: "Sony", 
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Sony_logo.svg/500px-Sony_logo.svg.png",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Sony_logo.svg",
       heightClass: "h-6 md:h-8" 
     },
     { 
       name: "Samsung", 
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/500px-Samsung_Logo.svg.png",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg",
       heightClass: "h-8 md:h-10" 
     },
     { 
       name: "LG", 
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/LG_logo_%282015%29.svg/500px-LG_logo_%282015%29.svg.png",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/b/bf/LG_logo_%282015%29.svg",
       heightClass: "h-10 md:h-12" 
     },
     { 
       name: "Panasonic", 
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Panasonic_logo_%28Blue%29.svg/500px-Panasonic_logo_%28Blue%29.svg.png",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/0/07/Panasonic_logo_%28Blue%29.svg",
       heightClass: "h-6 md:h-8" 
     },
     { 
       name: "OnePlus", 
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/OnePlus_logo_2020.svg/500px-OnePlus_logo_2020.svg.png",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/f/f8/OnePlus_logo_2020.svg",
       heightClass: "h-6 md:h-8" 
     }
   ];
@@ -46,7 +46,9 @@ export const BrandSection: React.FC = () => {
               <img 
                 src={brand.logo} 
                 alt={`${brand.name} TV Repair`} 
-                className={`w-auto max-w-[80%] object-contain opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300 filter grayscale group-hover:grayscale-0 ${brand.heightClass}`}
+                loading="lazy"
+                referrerPolicy="no-referrer"
+                className={`w-auto max-w-full object-contain opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300 filter grayscale group-hover:grayscale-0 ${brand.heightClass}`}
               />
             </div>
           ))}
